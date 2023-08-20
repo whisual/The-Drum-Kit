ba962a05529b99089fadcfd5b5fdd5a5dfe079c4
Vishal Sharma <102051680+whisual@users.noreply.github.com>
2023-08-20 17:35:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+function sound1() {
+    var snd = new Audio('mixkit-achievement-win-drums-555.wav')
+    snd.play()//plays the sound
+}
+function sound2() {
+    var snd = new Audio('mixkit-deep-dark-horror-drum-557.wav')
+    snd.play()
+}
+function sound3() {
+    var snd = new Audio('mixkit-achievement-win-drums-555.wav')
+    snd.play()
+}
+function sound4() {
+    var snd = new Audio('mixkit-bad-joke-drums-2893 (1).wav')
+    snd.play()
+}
+function sound5() {
+    var snd = new Audio('mixkit-guitar-notification-alert-2320.wav')
+    snd.play()
+}
+function sound6() {
+    var snd = new Audio('mixkit-clown-horn-at-circus-715.wav')
+    snd.play()
+}
